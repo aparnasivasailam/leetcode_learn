8ea304bcfca05f369352c98c0bad840a29e41316
Aparna Sivasailam <aparnaceg@gmail.com>
2024-08-21 19:21:48 +0530

    Finished first problem

---
+++ beginner_guide/add_two_numbers/AddTwoNumbers.java
@@ -0,0 +1,19 @@
+package beginner_guide.add_two_numbers;
+
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        System.out.println("Please enter the numbers: ");
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int y = in.nextInt();
+        int result = callAddTwoNumbers(x, y);
+        System.out.println("Sum of the numbers: " + result);
+    }
+
+    public static int callAddTwoNumbers(int x, int y) {
+        return x + y;
+    }
+    
+}
